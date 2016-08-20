90782a037af3df560cb4e6c481013ac735e47c23
Vecchi21 <karinamsv16@hotmail.com>
2016-08-20 20:09:57 -0300

    aula1

---
+++ body.php
@@ -0,0 +1,5 @@
+<body>
+  <?php
+  echo $variavel; //imprimir
+  ?>
+</body>
--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-hello word!
+<?php
+	$variavel = "hello word!"; //variavel
+	define("CONSTANTE", "hello word!"); //constante
+	const CONSTANTE2 = "hello word!"; //não é global, nem mutavel
+ ?>
+
+<!DOCTYPE html>
+<html>
+	<?php
+		require_once "head.php"; //não deixa repetir a saida
+		include "body.php";// deixa repetir a saida
+	?>
+</html>
